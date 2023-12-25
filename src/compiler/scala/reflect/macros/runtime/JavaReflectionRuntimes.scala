fabc468bec32d9cfe78baf5301bdd14f1fa2326e
friendseeker <66892505+Friendseeker@users.noreply.github.com>
2023-12-24 21:32:18 -0800

    Fix compiler error

--- src/compiler/scala/reflect/macros/runtime/JavaReflectionRuntimes.scala
+++ src/compiler/scala/reflect/macros/runtime/JavaReflectionRuntimes.scala
@@ -25,7 +25,6 @@ trait JavaReflectionRuntimes {
     self: MacroRuntimeResolver =>
 
     def resolveJavaReflectionRuntime(classLoader: ClassLoader): MacroRuntime = {
-      def saveContext(expandee: Tree, context: MacroContext): Unit = expandee.updateAttachment(MacroContextAttachment(context))
       val implClass = Class.forName(className, true, classLoader)
       val implMeths = implClass.getMethods.find(_.getName == methName)
       // relies on the fact that macro impls cannot be overloaded
@@ -42,14 +41,14 @@ trait JavaReflectionRuntimes {
 
         val Array(bundleCtor) = implClass.getConstructors.filter(isBundleCtor): @unchecked
         args => {
-          saveContext(args.c.expandee, args.c)
+          args.c.expandee.updateAttachment(MacroContextAttachment(args.c))
           val implObj = bundleCtor.newInstance(args.c)
           implMeth.invoke(implObj, args.others.asInstanceOf[Seq[AnyRef]]: _*)
         }
       } else {
         val implObj = ReflectionUtils.staticSingletonInstance(implClass)
         args => {
-          saveContext(args.c.expandee, args.c)
+          args.c.expandee.updateAttachment(MacroContextAttachment(args.c))
           val implArgs = args.c +: args.others
           implMeth.invoke(implObj, implArgs.asInstanceOf[Seq[AnyRef]]: _*)
         }
